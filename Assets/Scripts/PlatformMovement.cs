ff7f3d27661af155d6bff7d80ecc4d5bc844aef0
sinanavcik <sinanvck@gmail.com>
2020-04-14 21:53:05 +0300

    Added RandomMovement Script and some configurations

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public NavMeshAgent nav;
+    public Transform leftCorner;
+    public Transform rightCorner;
+    public Transform upCorner;
+    public Transform downCorner;
+    Vector3 target;
+    public float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        SetTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 6f)
+        {
+            SetTarget();
+            timer = 0;
+        }
+
+
+    }
+    void SetTarget()
+    {
+        float xPos = Random.Range(leftCorner.position.x, rightCorner.position.x);
+        float zPos = Random.Range(downCorner.position.z, upCorner.position.z);
+        target = new Vector3(xPos, transform.position.y, zPos);
+        nav.SetDestination(target);
+    }
+}
